f939ef17a4b6a7d83ce8e4906d0b78e616b04a39
gavinquach <gvqch8@gmail.com>
2023-07-14 20:47:10 +0700

    fixed top half loading later than bottom half

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,8 @@
 import dynamic from "next/dynamic";
 import { FC } from "react";
 import { SidebarWrapper } from "./SidebarStyled.ts";
-
+import { DirectoryTree } from "./DirectoryTree.tsx";
 const BottomHalf = dynamic(() => import("./BottomHalf/BottomHalf.tsx").then((mod) => mod.BottomHalf));
-const DirectoryTree = dynamic(() => import("./DirectoryTree.tsx").then((mod) => mod.DirectoryTree));
 
 export const Sidebar: FC = () => (
     <SidebarWrapper>
